da85702df0e637dcc0fe85c35370598a9700dfd6
Maximilien Sock <maximilien.sock@edu.ece.fr>
2017-10-24 09:07:55 +0200

    Data getters and setters
    Get and set data from the database and display it on the screen

--- webarena/src/Model/Table/FightersTable.php
+++ webarena/src/Model/Table/FightersTable.php
@@ -12,7 +12,152 @@ class FightersTable extends Table
   }
   public function getFighterName($playerid)
   {
-      $query = $this->find('list')->where(["Fighters.id" => $playerid])->first();
-      return($query);
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $name = $query->get("name");
+      return($name);
   }
+  public function getFighterId($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $id = $query->get("id");
+      return($id);
+  }
+  public function getFighterLevel($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $level = $query->get("level");
+      return($level);
+  }
+  public function getFighterCoordX($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $coordx = $query->get("coordinate_x");
+      return($coordx);
+  }
+  public function getFighterCoordY($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $coordy = $query->get("coordinate_y");
+      return($coordy);
+  }
+  public function getFighterXp($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $xp = $query->get("xp");
+      return($xp);
+  }
+  public function getFighterSight($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $sight = $query->get("skill_sight");
+      return($sight);
+  }
+  public function getFighterStrength($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $strength = $query->get("skill_strength");
+      return($strength);
+  }
+  public function getFighterHealth($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $health = $query->get("skill_health");
+      return($health);
+  }
+  public function getFighterCurrentHealth($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $currentHealth = $query->get("current_health");
+      return($currentHealth);
+  }
+  public function getFighterNextActionTime($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $nextactiontime = $query->get("next_action_time");
+      return($nextactiontime);
+  }
+  public function getFighterGuildId($playerid)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $guildId = $query->get("guild_id");
+      return($guildId);
+  }
+  public function SetFighterName($playerid,$newfightername)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->name=$newfightername;
+      $this->save($query);
+      
+  }
+  public function SetFighterCoordx($playerid,$depx)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->coordinate_x=$query->coordinate_x+$depx;
+      $this->save($query);
+      
+  }
+  public function SetFighterCoordy($playerid,$depy)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->coordinate_y=$query->coordinate_y+$depy;
+      $this->save($query);
+      
+  }
+  public function SetFighterLevel($playerid,$NewFighterLevel)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->level=$NewFighterLevel;
+      $this->save($query);
+      
+  }
+  public function SetFighterXp($playerid,$NewFighterXp)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->xp=$NewFighterXp;
+      $this->save($query);
+      
+  }
+  public function SetFighterSight($playerid,$NewFighterSight)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->skill_sight=$NewFighterSight;
+      $this->save($query);
+      
+  }
+  public function SetFighterStrength($playerid,$NewFighterStrength)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->skill_strength=$NewFighterStrength;
+      $this->save($query);
+      
+  }
+  public function SetFighterHealth($playerid,$NewFighterCurrentHealth)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->skill_health=$NewFighterCurrentHealth;
+      $this->save($query);
+      
+  }
+  public function SetFighterCurrentHealth($playerid,$NewFighterHealth)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->current_health=$NewFighterHealth;
+      $this->save($query);
+      
+  }
+  public function SetFighterCurrentNextActionTime($playerid,$NewFighterNextActionTime)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->next_action_time=$NewFighterNextActionTime;
+      $this->save($query);
+      
+  }
+  public function SetFighterCurrentGuildId($playerid,$NewFighterGuildId)
+  {
+      $query = $this->find('all')->where(["Fighters.id" => $playerid])->first();
+      $query->guild_id=$NewFighterGuildId;
+      $this->save($query);
+      
+  }
+  
 }
--- webarena/src/Template/Arenas/fighter.ctp
+++ webarena/src/Template/Arenas/fighter.ctp
@@ -1,1 +1,12 @@
-name : <?php echo $FighterName;?>
+Id : <?php echo $FighterId;?> <br>
+Name : <?php echo $FighterName;?> <br>
+Level : <?php echo $FighterLevel;?> <br>
+CoordX : <?php echo $FighterCoordX;?> <br>
+CoordY : <?php echo $FighterCoordY;?> <br>
+Xp : <?php echo $FighterXp;?> <br>
+Sight : <?php echo $FighterSight;?> <br>
+Strength : <?php echo $FighterStrength;?> <br>
+Health : <?php echo $FighterHealth;?> <br>
+Current Health : <?php echo $FighterCurrentHealth;?> <br>
+Next Action Time : <?php echo $FighterNextActionTime;?> <br>
+Guild Id : <?php echo $FighterGuildId;?> <br>
